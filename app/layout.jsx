ed6832b5d49659c3caf025d32f1a28e4770983c1
john keru <keruk355@gmail.com>
2023-05-21 22:25:45 +0800

    new feature added

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ export const metadata = {
   title: "Home",
   description: "Welcome to Mobile Legends",
   url: "https://github.com/imnotmeee",
-  image: `${process.env.HOST}/assets/images/3333.jpg`,
+  image: `${process.env.HOST}/assets/images/4.jpg`,
   twitterUsername: "@next_boilerplate",
 };
 
