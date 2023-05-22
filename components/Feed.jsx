5060bcc07da11b50df41fc06ce40389cb48eba90
john keru <keruk355@gmail.com>
2023-05-22 18:35:25 +0800

    new feature added

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -26,7 +26,7 @@ const Feed = () => {
   const [searchedResults, setSearchedResults] = useState([]);
 
   const fetchPosts = async () => {
-    const response = await fetch("/api/prompt", { next: { revalidate: 20 } });
+    const response = await fetch("/api/prompt");
     const data = await response.json();
     setAllPosts(data);
   };
