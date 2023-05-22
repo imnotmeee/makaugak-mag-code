c91da282e8440e7619bb32cc64224739a923398a
john keru <keruk355@gmail.com>
2023-05-22 22:15:44 +0800

    cool

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,33 @@
 import Feed from "@components/Feed";
 
-const Home = async () => {
-  const response = await fetch(`${process.env.HOST}/api/prompt`, {
-    cache: "no-store",
+const fetchPosts = async () => {
+  const response = await fetch(`https://fakestoreapi.com/products`, {
+    next: {
+      revalidate: 5,
+    },
   });
+  console.log("run every fucking time");
+  return response.json();
+};
 
-  return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        Discover & Share
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
-      </h1>
-      <p className="desc text-center">
-        Promptopia is an open-source AI prompting tool for modern world to
-        discover, create and share creative prompts
-      </p>
+const Home = async () => {
+  const posts = await fetchPosts();
 
-      <Feed allPosts={await response.json()} />
-    </section>
-  );
+  return <section className="w-full flex-center flex-col">fefe</section>;
 };
 
 export default Home;
+
+{
+  /* <h1 className="head_text text-center">
+Discover & Share
+<br className="max-md:hidden" />
+<span className="orange_gradient text-center"> AI-Powered Prompts</span>
+</h1>
+<p className="desc text-center">
+Promptopia is an open-source AI prompting tool for modern world to
+discover, create and share creative prompts
+</p>
+
+<Feed allPosts={await response.json()} /> */
+}
